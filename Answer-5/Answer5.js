2c544cc1284fd4170656a715f6cc3623c0f4aadf
Vijay <hello.vijayj@gmail.com>
2024-02-15 17:36:14 +0530

    Added solutions to all questions

---
+++ Answer-5/Answer5.js
@@ -0,0 +1,36 @@
+const xml2js = require('xml2js');
+const fs = require('fs');
+try {
+  const xmlContents = fs.readFileSync('Question-1/Question-1.xml', 'utf-8');
+
+  if(xmlContents) {
+    xml2js.parseString(xmlContents, (err, data) => {
+    if(err) {
+      console.log(err);
+      process.exit(1);
+    }
+    
+    if(data) {
+      data?.library?.book.forEach(item => {
+        item.title = "Test";
+      });
+      data.library.book.forEach((item) => {
+        if(item !== undefined) {
+          console.log(item);
+        }
+      });
+      const builder = new xml2js.Builder();
+      const xmlDest = builder.buildObject(data);
+      fs.writeFile('Question-5/Question-5-Title-Updated.xml', xmlDest,(err) => {
+        if(err) {
+          process.exit(1);
+        }
+        console.log('All titles updated to Test');
+      })
+    }
+  })
+  }
+
+} catch(err) {
+  console.log(err);
+}
